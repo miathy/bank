0dfca0ed92c3e8bbcd421fd93a173459cf185101
Mai <mainguyen15695@gmail.com>
2021-02-18 07:03:13 +1100

    class Address

---
+++ server/Address.py
@@ -0,0 +1,62 @@
+class Address:
+    def __init__(self):
+         #street number: numbers only < 1000
+        #street name: only letters, capitalised first letter of each word
+        #suburb: only letters, capitalised first letter of each word
+        #state: choose from Australian States only
+        #Post code: numbers only, 4 digits long
+        self.streetNumber = streetNumber
+        self.streetName = streetNumber
+        self.suburb = suburb
+        self.state = state   
+        self.postCode = postCode
+    
+    def setStreetNumber(self, streetNumber):
+        if any(char.isalpha() for char in streetNumber):
+            print('Street number contains letters, Please enter again')
+        elif len(str(streetName)) > 1000:
+            print('Street number must be less than 1000 digits')
+        else:
+            self.streetNumber = streetNumber
+            print('Street number is set to' + self.streetNumber)
+    
+    def setStreetName(self, streetName):
+        if any(char.isdigit() for char in streetName):
+            print("Name contains Digits, Please enter again")
+        elif any(char[0].islower() for char[0] in streetName.split())
+            print('Please capitalize 1st letter of each word')
+        else:
+            self.streetName = streetName
+            print("Street Name set to " + self.streetName)
+    
+    def setSuburb(self, suburb):
+        if any(char.isdigit() for char in suburb):
+            print("Name contains Digits, Please enter again")
+        elif suburb not title:
+            print('Please capitalize 1st letter of each word')
+        else:
+            self.suburb = suburb
+            print("Street Name set to " + self.suburb)
+    
+    def setState(self, state):
+        states = ['VIC', 'QLD', 'NT', 'TAS', 'NSW', 'WA','SA','ACT']
+        if state not in states:
+            print('please enter Australian State only')
+        else:
+            self.state = state
+    
+    def setpostCode(self, postCode):
+        if any(char.isalpha() for char in postCode):
+            print('Post code contains letters, Please enter again')
+        elif len(str(postCode)) !=4:
+            print('Street number must have 4 digits, please enter again')
+        else:
+            self.postCode = postCode
+            print('post code is set to' + self.postCode)
+            
+
+
+
+
+
+           
--- server/User.py
+++ server/User.py
@@ -18,12 +18,33 @@ class User:
             self.firstName = firstName
             print("First Name set to " + self.firstName)
 
-    def setLastName():
-        return
+    def setLastName(self, lastName):
         #Ensure Capital first letter, only letters, no spaces
+        if any(char.isdigit() for char in lastName):
+            print("Name contains Digits, Please enter again")
+        elif any(char == ' ' for char in lastName):
+            print("Name contains Space! Please enter again")
+        elif (lastName[0].islower()):
+            print("Captilise first letter!!! Try Again!!")
+        else:
+            self.lastName = lastName
+            print("Last Name set to " + self.lastName)
 
-    def setPhoneNumber():
-        return
+    def setPhoneNumber(self, phoneNumber):
+        if any(char.isalpha() for char in phoneNumber):
+            print('Phone number contains letters, Please enter again')
+        elif phoneNumber[0] != 0:
+            print('Phone number must start with 0, Please enter again')
+        elif len(str(phoneNumber)) > 10:
+            print('Phone number contains 10 digits only, please enter again')
+        elif len(str(phoneNumber)) < 10:
+            print('Phone number contains 10 digits only, please enter again')
+        else:
+            self.phoneNumber = phoneNumber
+            print("Phone Number set to " + self.phoneNumber)
+
+        
+
         #Ensure numbers only, 10 digits, starts with 0
 
     def setEmailAddress():
@@ -42,7 +63,9 @@ class User:
 
 
 user = User()
-user.setFirstName("mai1")
-user.setFirstName("ma i")
-user.setFirstName("mai")
-user.setFirstName("Mai")
+#user.setFirstName("mai1")
+#user.setFirstName("ma i")
+#user.setFirstName("mai")
+#user.setFirstName("Mai")
+user.setPhoneNumber('123456')
+user.setPhoneNumber('a42356')
